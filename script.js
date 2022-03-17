08996cd03a976bab3da692b25f5310cf695aaa74
boualemdai <boualemdai@gmail.com>
2022-03-17 11:54:04 +0100

    Bank-Website

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 const header = document.querySelector("header");
 
 const navbar = document.querySelector("nav");
-const secondSection=document.querySelector(".second-section")
-const line=document.querySelector(".line")
-const firstSection=document.querySelector(".first-section")
-console.log(firstSection.scrollHeight);
-//////event listner
-
+const secondSection = document.querySelector(".second-section");
+const line1 = document.querySelector(".l1");
+const line2 = document.querySelector(".l2");
+const firstSection = document.querySelector(".first-section");
+const btns = document.querySelectorAll(".btn");
+const cir = document.querySelector(".cir");
+const thirdSection = document.querySelector(".third-section");
+const blabla = document.querySelector(".blabla");
 
 Array.from(navbar.children).forEach((el) => {
   if (el !== header) {
@@ -27,20 +29,46 @@ Array.from(navbar.children).forEach((el) => {
   }
 });
 
-window.addEventListener("scroll",()=>{
-let y=window.scrollY
-if(y>400){
-   
-secondSection.classList.add("visible")
-line.classList.add("visible")
+window.addEventListener("scroll", () => {
+  let y = window.scrollY;
+  if (y > firstSection.scrollHeight * 0.6) {
+    secondSection.classList.add("visible");
+    line1.classList.add("visible");
+  }
+  if (y >= firstSection.scrollHeight - navbar.clientHeight - 1) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (y > firstSection.scrollHeight + secondSection.scrollHeight * 0.7) {
+    thirdSection.classList.add("visible");
+    line2.classList.add("visible");
+  }
+});
 
-}
-if(y>=firstSection.scrollHeight-navbar.clientHeight-1){
-    
-    navbar.classList.add("fixed-nav")
-}else{
-    navbar.classList.remove("fixed-nav")
-    
+Array.from(btns).forEach((e) => {
+  e.addEventListener("click", () => {
+    cir.style.backgroundColor = window.getComputedStyle(e)["background-color"];
+    for (let i = 1; i < 4; i++) {
+      if (
+        document.querySelectorAll(`.a${i}`) !==
+        document.querySelectorAll(`.a${parseInt(e.textContent)}`)
+      ) {
+        document
+          .querySelectorAll(`.a${i}`)
+          .forEach((el) => el.classList.add("hid"));
+      }
+    }
+    document.querySelectorAll(`.a${parseInt(e.textContent)}`).forEach((e) => {
+      e.classList.remove("hid");
+    });
 
-}
-})
+    Array.from(btns).forEach((el) => {
+      if (el === e) {
+        el.style.transform = "translateY(25%)";
+      } else {
+        el.style.transform = "translateY(50%)";
+      }
+    });
+  });
+});
